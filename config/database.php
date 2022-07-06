c3f2ddad2f5b06e98eb99b1ea76e5c9bebeea211
Ilya Kuznetsov <kuzyxd@yandex.ru>
2022-07-06 11:45:07 +0500

    Создано соединение для дампа ЕГАИС'а
    Экспортировал дамп и импортировал через psql вручную

--- config/database.php
+++ config/database.php
@@ -78,6 +78,21 @@ return [
             'sslmode' => 'prefer',
         ],
 
+        'pgsql_dump' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_DUMP', '127.0.0.1'),
+            'port' => env('DB_PORT_DUMP', '5432'),
+            'database' => env('DB_DATABASE_DUMP', 'forge'),
+            'username' => env('DB_USERNAME_DUMP', 'forge'),
+            'password' => env('DB_PASSWORD_DUMP', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
