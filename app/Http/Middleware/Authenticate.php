fe4f58c860f73305fade79159e932d0cbbf21a0e
Ilya Kuznetsov <kuzyxd@yandex.ru>
2022-07-25 11:42:13 +0500

    Процесс авторизации, добавление начальных гуардов клиента и менеджера
    начальные гуарды нужны для разграничения доступа к страницам и действиям

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,8 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (!$request->expectsJson() && $request->route()->getPrefix() == '/manager') {
+            return route('manager.login');
         }
     }
 }
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class Client extends Model
+class Client extends Authenticatable
 {
     use HasFactory;
 }
