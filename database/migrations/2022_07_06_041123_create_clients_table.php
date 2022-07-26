33cdf196be7e144586b7fc4cdf8e68d4cd8b14fb
Ilya Kuznetsov <kuzyxd@yandex.ru>
2022-07-26 12:16:45 +0500

    Добавил "комментарий" к пользователям
    ... чтобы помечать кто к какому филиалу принадлежит

--- database/migrations/2022_07_06_041123_create_clients_table.php
+++ database/migrations/2022_07_06_041123_create_clients_table.php
@@ -4,21 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClientsTable extends Migration
-{
+class CreateClientsTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('fio');
             $table->string('password');
             $table->string('certificate_serial_number', 50);
             $table->timestamp('certificate_expire_to_date')->nullable();
+            $table->string('note')->nullable();
 
             $table->softDeletes();
             $table->timestamps();
@@ -30,8 +29,7 @@ class CreateClientsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('clients');
     }
 }
