e48052d57e8d05dced46e0c0d6f729e5ca025901
Ilya Kuznetsov <kuzyxd@yandex.ru>
2022-07-26 12:17:48 +0500

    Сид пользователей из дампа в новую базу
    берет только активных пользователей, генерирует им пароли случайно и сохраняет пароли в файл в storage/app

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,9 +5,8 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Client extends Authenticatable
-{
+class Client extends Authenticatable {
     use HasFactory;
 
-    protected $fillable = ['fio', 'certificate_serial_number', 'password'];
+    protected $guarded = [];
 }
--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -2,17 +2,41 @@
 
 namespace Database\Seeders;
 
+use App\Services\Auth\Client;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
-class ClientSeeder extends Seeder
-{
+class ClientSeeder extends Seeder {
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(Client $clientService) {
+        $dumpTable = DB::connection('pgsql_dump')->table('users')->get(['created_at', 'updated_at',
+                'first_name', 'last_name', 'middle_name', 'certificate_serial',
+                'certificate_expire_to_date', 'status'])->where('status', '=', 'ACTIVE');
+
+        Storage::put('clients.txt', '');
+        $dumpTable->each(function ($user) use ($clientService) {
+            $dataToSave['fio'] = $user->last_name . ' ' . $user->first_name . ' ' . $user->middle_name;
+            $dataToSave['password'] = Str::random();
+            $dataToSave['created_at'] = date('Y-m-d h:i:s', strtotime($user->created_at));
+            $dataToSave['updated_at'] = $user->updated_at ? date('Y-m-d h:i:s', strtotime($user->updated_at)):null;
+            $dataToSave['certificate_serial_number'] = $user->certificate_serial;
+            $dataToSave['certificate_expire_to_date'] =
+                    date('Y-m-d h:i:s', strtotime($user->certificate_expire_to_date));
+
+            if ($clientService->create($dataToSave)) {
+                Storage::append('clients.txt', $dataToSave['certificate_serial_number'] . ' ' . $dataToSave['fio'] . ' ' . $dataToSave['password']);
+            } else {
+                dump('Не удалось сохранить:');
+                dump($dataToSave);
+            }
+        });
+
+
     }
 }
