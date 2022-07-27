be9b3ffb13b32b84189d500aea153718b553c08a
Ilya Kuznetsov <kuzyxd@yandex.ru>
2022-07-27 14:52:17 +0500

    Правка кода для редиректа

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,21 +3,21 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
-class Authenticate extends Middleware
-{
+class Authenticate extends Middleware {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return string|null
      */
-    protected function redirectTo($request)
-    {
-        if (!$request->expectsJson() && $request->route()->getPrefix() == '/manager') {
+    protected function redirectTo($request) {
+        if (!$request->expectsJson() && str_contains($request->route()->getPrefix(), 'manager')) {
             return route('manager.login');
         }
-        if (!$request->expectsJson() && $request->route()->getPrefix() == '/client') {
+
+        if (!$request->expectsJson() && str_contains($request->route()->getPrefix(), 'client')) {
             return route('client.login');
         }
     }
