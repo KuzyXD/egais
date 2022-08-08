66d0497d7ec5309252a31583fef2966fad210281
Ilya Kuznetsov <kuzyxd@yandex.ru>
2022-08-08 10:46:56 +0500

    Редирект на авторизацию, если получаем 401

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,19 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.withCredentials = true;
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.status === 401) {
+        document.location.href = '/manager/login';
+    }
+
+    return Promise.reject(error);
+});
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
