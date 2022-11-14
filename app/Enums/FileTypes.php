f2b5d55bde298ceca11bdcd8de9e540fe22fbdee
KuzyXD <kuzyxd@yandex.ru>
2022-11-14 13:35:21 +0500

    Фикс Enum'ов
    * Они должны были идти в PhpDOC'е с скобками для правильной работы

--- app/Enums/FileTypes.php
+++ app/Enums/FileTypes.php
@@ -6,16 +6,16 @@ use Spatie\Enum\Enum;
 
 
 /**
- * @method static self PASSPORT
- * @method static self PHOTO
- * @method static self SNILS
- * @method static self REQUEST
- * @method static self APPLICATION
- * @method static self PROCURATION
- * @method static self ZIP
- * @method static self SIGNED_ZIP
- * @method static self CERTIFICATE_BLANK
- * @method static self CERTIFICATE
+ * @method static self PASSPORT()
+ * @method static self PHOTO()
+ * @method static self SNILS()
+ * @method static self REQUEST()
+ * @method static self APPLICATION()
+ * @method static self PROCURATION()
+ * @method static self ZIP()
+ * @method static self SIGNED_ZIP()
+ * @method static self CERTIFICATE_BLANK()
+ * @method static self CERTIFICATE()
  */
 class FileTypes extends Enum
 {
